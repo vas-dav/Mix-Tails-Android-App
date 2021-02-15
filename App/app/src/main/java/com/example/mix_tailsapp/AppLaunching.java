d178047972d06043a050e7583a784f9574cc8df1
An Huynh <An.Huynh@metropolia.fi>
2021-02-15 18:44:51 +0200

    added sign up form
    sign up form design and code (for opening activity) added

--- App/app/src/main/java/com/example/mix_tailsapp/AppLaunching.java
+++ App/app/src/main/java/com/example/mix_tailsapp/AppLaunching.java
@@ -5,18 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AppLaunching extends AppCompatActivity {
-
+    private EditText name, email, password, comfirm_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_launching);
 
+        //casting the edit text variables to their ids
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        comfirm_password = findViewById(R.id.comfirm_password);
+
+        //Create onClick method for sign up button to open the sign up form
+        Button signupBtn = findViewById(R.id.signupBtn);
+        signupBtn.setOnClickListener(v -> {
+
+            //open activity
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+
+        });
     }
+
+
     public void decide (View view){
         Intent questions = new Intent(AppLaunching.this, QuestionSpinner.class);
         startActivity(questions);
     }
+
 
 }
--- App/app/src/main/java/com/example/mix_tailsapp/SignupActivity.java
+++ App/app/src/main/java/com/example/mix_tailsapp/SignupActivity.java
@@ -2,13 +2,23 @@ package com.example.mix_tailsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+    public static final String EXTRA_NAME = "com.example.mix_tailsapp.EXTRA_NAME";
+    public static final String EXTRA_EMAIL = "com.example.mix_tailsapp.EXTRA_EMAIL";
+    public static final String EXTRA_PASS = "com.example.mix_tailsapp.EXTRA_PASS";
+    public static final String EXTRA_CONFIRMPASS = "com.example.mix_tailsapp.EXTRA_CONFIRMPASS";
 
+    private EditText name, email, password, comfirm_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Intent intent = getIntent();
     }
 }
