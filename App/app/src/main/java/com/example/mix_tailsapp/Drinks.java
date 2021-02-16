919fd0a7ee2a37ea29c244003a6c77b68cfef542
migiFi <miguela@metropolia.fi>
2021-02-16 14:09:38 +0200

    Added 19 new drinks
    To the Drinks.java class

--- App/app/src/main/java/com/example/mix_tailsapp/Drinks.java
+++ App/app/src/main/java/com/example/mix_tailsapp/Drinks.java
@@ -19,6 +19,26 @@ public class Drinks {
     DrinksData WhiskeySour = new DrinksData("Whiskey Sour", "Whiskey", "L", "Sour", "Mild");
     DrinksData Bacardi = new DrinksData("Bacardi", "Rum", "M", "Fresh", "Mild");
     DrinksData DryMartini = new DrinksData("Dry Martini", "Gin", "M", "Boozy", "Strong");
+    DrinksData Negroni = new DrinksData("Negroni","Gin", "M", "Bitter Sweet", "Mild");
+    DrinksData Boulevardier = new DrinksData("Boulevardier", "Whiskey", "S", "Bitter sweet", "Strong");
+    DrinksData OldFashioned = new DrinksData("Old Fashioned", "Whiskey", "M", "Boozy", "Strong");
+    DrinksData BloodAndSand = new DrinksData("Blood and Sand", "Whiskey", "M", "Sweet", "Mild");
+    DrinksData CynarTown = new DrinksData("CynarTown", "Gin", "L", "Bitter sweet", "Strong");
+    DrinksData French75 = new DrinksData("French 75", "Gin", "M", "Fresh", "Soft");
+    DrinksData WhiteLady = new DrinksData("White lady", "Gin", "M", "Fresh", "Soft");
+    DrinksData Cosmopolitan = new DrinksData("Cosmopolitan", "Vodka", "S", "Fresh", "Mild");
+    DrinksData ScrewDriver = new DrinksData("Screwdriver", "Vodka", "L", "Fresh", "Soft");
+    DrinksData SideCar = new DrinksData("Sidecar", "Cognac", "M", "Fresh", "Soft");
+    DrinksData GinFizz = new DrinksData("Gin Fizz", "Gin", "L", "Fresh", "Soft");
+    DrinksData JohnCollins = new DrinksData("John Collins", "Gin", "L", "Fresh", "Soft");
+    DrinksData TomCollins = new DrinksData("Tom Collins", "Gin", "L", "Fersh", "Soft");
+    DrinksData RustyNail = new DrinksData("Rusty nail", "Whiskey", "S", "Boozy", "Strong");
+    DrinksData HarveyWallbanger = new DrinksData("Harvey Wallbanger", "Vodka", "L", "Fresh", "Mild");
+    DrinksData Gimlet = new DrinksData("Gimlet", "Gin", "S", "Sour", "Mild");
+    DrinksData TwentiethCentury = new DrinksData("Twentieth Century", "Gin", "M", "Fresh", "Soft");
+    DrinksData IrishCoffee = new DrinksData("Irish coffee", "Whiskey", "L", "Boozy", "Strong");
+    DrinksData Margarita = new DrinksData("Margarita", "Tequila", "L", "Fresh", "Mild");
+
 
     private List<DrinksData> cocktail;
 
@@ -34,6 +54,25 @@ public class Drinks {
         cocktail.add(WhiskeySour);
         cocktail.add(Bacardi);
         cocktail.add(DryMartini);
+        cocktail.add(Negroni);
+        cocktail.add(Boulevardier);
+        cocktail.add(OldFashioned);
+        cocktail.add(BloodAndSand);
+        cocktail.add(CynarTown);
+        cocktail.add(French75);
+        cocktail.add(WhiteLady);
+        cocktail.add(Cosmopolitan);
+        cocktail.add(ScrewDriver);
+        cocktail.add(SideCar);
+        cocktail.add(GinFizz);
+        cocktail.add(JohnCollins);
+        cocktail.add(TomCollins);
+        cocktail.add(RustyNail);
+        cocktail.add(HarveyWallbanger);
+        cocktail.add(Gimlet);
+        cocktail.add(TwentiethCentury);
+        cocktail.add(IrishCoffee);
+        cocktail.add(Margarita);
     }
 
     //very first version of comparison algorithm (not tested yet)
