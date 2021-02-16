c7b4a88fb512ce688dcf41edcdf509448fe2719a
Vasily Davydov <vasily.davydov@metropolia.fi>
2021-02-16 19:02:50 +0200

    Return function for SignUpDone
    made a functional button in SignupConfirmationScreen.java with small changes in xml

--- App/app/src/main/java/com/example/mix_tailsapp/SignupConfirmationScreen.java
+++ App/app/src/main/java/com/example/mix_tailsapp/SignupConfirmationScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignupConfirmationScreen extends AppCompatActivity {
@@ -13,6 +15,12 @@ public class SignupConfirmationScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__confirmation_screen);
 
+        //A function for returning back on the mainMenu
+      Button returnButton = (Button)findViewById(R.id.endSubscription);
+      returnButton.setOnClickListener(v -> {
+          Intent goBack = new Intent(SignupConfirmationScreen.this, AppLaunching.class);
+          startActivity(goBack);
+      });
 
     }
 }
