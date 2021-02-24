a2da7815bdb4e3aa8822565171755ff4bf999612
An Huynh <An.Huynh@metropolia.fi>
2021-02-24 14:30:25 +0200

    Refactor activities names and edit constraint

--- App/app/src/main/java/com/example/mix_tailsapp/AppLaunching.java
+++ App/app/src/main/java/com/example/mix_tailsapp/AppLaunching.java
@@ -2,74 +2,27 @@ package com.example.mix_tailsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.os.Handler;
 
 public class AppLaunching extends AppCompatActivity {
 
-    //Declaring variables
-    SharedPreferences tempStorageGet, permStorageGet;
-    TextView welcomeText;
-
+    private static final String SURPRISE_KEY = "KEWIOhguyfbvUWIGefyuowUILGYUOAWGYEURFQU3";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //calling for both Storage on the AppLaunching Screen
-        tempStorageGet = getSharedPreferences(SignupActivity.TEMP_STORAGE, Activity.MODE_PRIVATE);
-        permStorageGet = getSharedPreferences(SignupActivity.PERM_STORAGE, Activity.MODE_PRIVATE);
+    public void onCreate(Bundle savedInstance) {
 
-
-        //Checking for userData in sharedPreferences and deciding which xml to use
-
-        //if userData is not empty, show a welcoming message
-        if (tempStorageGet.getBoolean(SignupActivity.SIGNED, false)) {
-            setContentView(R.layout.activity_app_launching_logged_in);
-            String name = tempStorageGet.getString(SignupActivity.EXTRA_NAME, "User");
-            welcomeText = (TextView) findViewById(R.id.welcomeBack);
-            welcomeText.setText("Welcome back, " + name);
-
-            //A function for deleting data from sharedPreferences (logging out)
-            Button logoutBtn = findViewById(R.id.logOut);
-            logoutBtn.setOnClickListener(v -> {
-
-                Intent logout = new Intent(this, MainActivity.class);
-                SharedPreferences.Editor deleter = tempStorageGet.edit();
-                deleter.clear();
-                if (deleter.commit()) {
-                    startActivity(logout);
-                }
-            });
-
-        }
-        //if userData is empty, then show activity with signIN/signUP
-        else {
-            setContentView(R.layout.activity_app_launching);
-            //Create onClick method for sign up button to open the sign up form
-            Button signupBtn = findViewById(R.id.signupBtn);
-            signupBtn.setOnClickListener(v -> {
-
-                //open activity
-                Intent intent = new Intent(this, SignupActivity.class);
-                startActivity(intent);
-
-            });
-
-
-        }
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_app_launching);
+        // App launching page with delay
+        new Handler().postDelayed(() -> {
+            Intent launchApp = new Intent(AppLaunching.this, MainActivity.class);
+            startActivity(launchApp);
+        }, 2000);
 
 
     }
 
 
-    //A function for "Let's Decide!" button to move you into QuestionSpinner
-    public void decide(View view) {
-        Intent questions = new Intent(AppLaunching.this, QuestionSpinner.class);
-        startActivity(questions);
-    }
 }
