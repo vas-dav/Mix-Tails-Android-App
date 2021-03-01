39e786761e9a4810f5bb19c4853b0b0eaf35c2d7
An Huynh <An.Huynh@metropolia.fi>
2021-03-01 16:05:10 +0200

    logOut Button in welcome page

--- App/app/src/main/java/com/example/mix_tailsapp/AppWelcomeScreen.java
+++ App/app/src/main/java/com/example/mix_tailsapp/AppWelcomeScreen.java
@@ -7,15 +7,19 @@ package com.example.mix_tailsapp;
  */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AppWelcomeScreen extends AppCompatActivity {
     //Declare variables
     private Button decideBtn, recommendBtn, drivingEstimation, favoriteBtn;
-
+    private ImageButton logOut;
+    private SharedPreferences tempStorageGet;
     //create an onClick listener when the buttons are clicked
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
@@ -50,5 +54,20 @@ public class AppWelcomeScreen extends AppCompatActivity {
         drivingEstimation.setOnClickListener(clickListener);
         favoriteBtn.setOnClickListener(clickListener);
 
+        //logOut Btn
+        logOut = findViewById(R.id.logOut);
+        tempStorageGet = getSharedPreferences(SignupActivity.TEMP_STORAGE, Activity.MODE_PRIVATE);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signOut = new Intent(AppWelcomeScreen.this,
+                        AppLaunching.class);
+                SharedPreferences.Editor deleter = tempStorageGet.edit();
+                deleter.clear();
+                if (deleter.commit()) {
+                    startActivity(signOut);
+                }
+            }
+        });
     }
 }
