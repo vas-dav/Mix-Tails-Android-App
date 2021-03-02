2495518c28686464316ed5490d3adaa810af81f2
An Huynh <An.Huynh@metropolia.fi>
2021-03-02 16:15:14 +0200

    Updated Add new drink activity

--- App/app/src/main/java/com/example/mix_tailsapp/AddingDrink.java
+++ App/app/src/main/java/com/example/mix_tailsapp/AddingDrink.java
@@ -11,18 +11,32 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
 public class AddingDrink extends AppCompatActivity {
-
+    private Button sendDrink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_drink);
-        //get Intent
-        Intent addDrink = getIntent();
+
+        //Button add drink onClickListener
+        sendDrink = findViewById(R.id.sendDrink);
+        sendDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(() -> {
+                    setContentView(R.layout.activity_add_drink_confirmation);
+                    Intent launchApp = new Intent(AddingDrink.this, DrinkRecommendationPage.class);
+                    startActivity(launchApp);
+                }, 2000);
+            }
+        });
+
     }
 }
