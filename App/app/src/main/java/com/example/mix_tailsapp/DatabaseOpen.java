2c6f368144c63b769d9906dc95b31bb30d0e30c2
An Huynh <An.Huynh@metropolia.fi>
2021-03-02 17:08:34 +0200

    commit

--- App/app/src/main/java/com/example/mix_tailsapp/DatabaseOpen.java
+++ App/app/src/main/java/com/example/mix_tailsapp/DatabaseOpen.java
@@ -9,8 +9,12 @@ public class DatabaseOpen extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "Drinks.db";
     private static final int DATABASE_VERSION = 1;
 
+
     public  DatabaseOpen(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
     }
+
+
+
 }
