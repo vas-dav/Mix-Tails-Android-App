cd5936bea7922e0493a59ff052ebd753c563aaaf
Vasily Davydov <vasily.davydov@metropolia.fi>
2021-03-04 20:40:58 +0200

    Create SoberDriver.java

---
+++ App/app/src/main/java/com/example/mix_tailsapp/SoberDriver.java
@@ -0,0 +1,11 @@
+package com.example.mix_tailsapp;
+
+
+//-------------------------------------------------------
+//This algorithm is based on the Univeristy of Michigan
+//Research and data taken from:
+//https://uhs.umich.edu/time-to-sober-up
+//-------------------------------------------------------
+
+public class SoberDriver {
+}
